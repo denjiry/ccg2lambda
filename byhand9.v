9ad839d955d700e208eca09cd2fb816646a61fa3
kento tajiri <k.denjiry@gmail.com>
2019-09-20 18:57:38 +0900

    Modify byhand9 to convert to Neo-Davidsonian Event Semantics

--- byhand9.v
+++ byhand9.v
@@ -5,4 +5,15 @@ Parameter _嫌う : Event -> Prop.
 Parameter _花 : Entity -> Prop.
 Parameter _青い : Event -> Prop.
 
-Theorem t1 :forall x, (_花(x) -> ((B(x) -> _嫌う(Alice, x)) \/ _好き(Alice, x))).
+Theorem t1 :forall x, (_花(x)
+                   ->
+                   ((exists e, ((_青い(e) /\ (Nom(e) = x))
+                            ->
+                            (_嫌う(e) /\ (Nom(e) = _アリス) /\ (Acc(e)=x))))
+                    \/
+                    exists e, (_嫌う(e) /\ (Nom(e) = _アリス) /\ (Acc(e)=x))
+                  )).
+Proof.
+Admitted.
+
+(* Theorem t2 :forall x, (_花(x) -> ((_青い(x) -> _嫌う(Alice, x)) \/ _好き(Alice, x))). *)
