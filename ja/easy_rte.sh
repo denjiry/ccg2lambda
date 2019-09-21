1c6d6e7fb65efd343c8f47bea3d8740d708c14d7
kento tajiri <k.denjiry@gmail.com>
2019-09-21 15:44:31 +0900

    Turn off cache on easy_rte.sh

--- ja/easy_rte.sh
+++ ja/easy_rte.sh
@@ -85,42 +85,35 @@ parser_cmd="java -Xmx4g -cp \"${parser_dir}/jar/*\" jigg.pipeline.Pipeline \
   -ccg.kBest 10 -file"
 
 # Syntactic parse the sentences into an XML file in $parsed_dir.
-if [ ! -e "${parsed_dir}/${sentences_basename}.jigg.xml" ]; then
-  # C&C parse and conversion into transccg format.
-  echo "Syntactic parsing ${sentences_fname}"
-  # This will create a file ${sentences_fname}.xml
-  eval $parser_cmd ${sentences_fname} \
-    > ${parsed_dir}/${sentences_basename}.log.std \
-    2> ${parsed_dir}/${sentences_basename}.log.err
-  mv ${sentences_fname}.xml ${parsed_dir}/${sentences_basename}.jigg.xml
-fi
+# C&C parse and conversion into transccg format.
+echo "Syntactic parsing ${sentences_fname}"
+# This will create a file ${sentences_fname}.xml
+eval $parser_cmd ${sentences_fname} \
+     > ${parsed_dir}/${sentences_basename}.log.std \
+     2> ${parsed_dir}/${sentences_basename}.log.err
+mv ${sentences_fname}.xml ${parsed_dir}/${sentences_basename}.jigg.xml
 
-if [ ! -e "${parsed_dir}/${sentences_basename}.xml" ]; then
-  cp ${parsed_dir}/${sentences_basename}.jigg.xml ${parsed_dir}/${sentences_basename}.xml
-fi
+cp ${parsed_dir}/${sentences_basename}.jigg.xml ${parsed_dir}/${sentences_basename}.xml
 
 # Semantic parsing the CCG trees in XML.
-if [ ! -e "$parsed_dir/${sentences_basename}.sem.xml" ]; then
-  echo "Semantic parsing $parsed_dir/${sentences_basename}.xml"
-  python scripts/semparse.py \
-    $parsed_dir/${sentences_basename}.xml \
-    $category_templates \
-    $parsed_dir/${sentences_basename}.sem.xml \
-    --arbi-types \
-    2> $parsed_dir/${sentences_basename}.sem.err
-fi
+echo "Semantic parsing $parsed_dir/${sentences_basename}.xml"
+python scripts/semparse.py \
+       $parsed_dir/${sentences_basename}.xml \
+       $category_templates \
+       $parsed_dir/${sentences_basename}.sem.xml \
+       --arbi-types \
+       2> $parsed_dir/${sentences_basename}.sem.err
 
 # Judge entailment with a theorem prover (Coq, at the moment).
-if [ ! -e "${results_dir}/${sentences_basename}.answer" ]; then
-  start_time=`python -c 'import time; print(time.time())'`
-  python scripts/prove.py \
-    $parsed_dir/${sentences_basename}.sem.xml \
-    --graph_out ${results_dir}/${sentences_basename}.html \
-    > ${results_dir}/${sentences_basename}.answer \
-    2> ${results_dir}/${sentences_basename}.err
-  end_time=`python -c 'import time; print(time.time())'`
-  echo "${end_time} - ${start_time}" | \
+start_time=`python -c 'import time; print(time.time())'`
+python scripts/prove.py \
+       $parsed_dir/${sentences_basename}.sem.xml \
+       --graph_out ${results_dir}/${sentences_basename}.html \
+       > ${results_dir}/${sentences_basename}.answer \
+       2> ${results_dir}/${sentences_basename}.err
+end_time=`python -c 'import time; print(time.time())'`
+echo "${end_time} - ${start_time}" | \
     bc -l | awk '{printf("%.2f\n", $1)}' \
-    > ${results_dir}/${sentences_basename}.time
-fi
+                > ${results_dir}/${sentences_basename}.time
+
 echo "Judged entailment for $parsed_dir/${sentences_basename}.sem.xml "`cat ${results_dir}/${sentences_basename}.answer`
