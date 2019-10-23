d9c780899c3b3e20c2b77c6cc9fb13d0e24e5901
kento tajiri <k.denjiry@gmail.com>
2019-10-23 19:12:22 +0900

    Impl rush construction of Main.elm

--- elm/Main.elm
+++ elm/Main.elm
@@ -1,5 +1,78 @@
+module Main exposing (main)
+
 import Browser
+import Html exposing (..)
+import Html.Attributes exposing (..)
+
+
+type Model
+    = Failure
+    | Processing
+    | Success
+
+
+type Msg
+    = FetchTable
 
 
 
-main = Browser.application
+--init
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( Success, Cmd.None )
+
+
+
+--update
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        FetchTable ->
+            ( Success, Cmd.none )
+
+
+
+-- subscriptions
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+
+-- view
+
+
+view : Model -> Html Msg
+view model =
+    case model of
+        Success ->
+            div []
+                [ text "success" ]
+
+        Processing ->
+            div []
+                [ text "processing" ]
+
+        Failure ->
+            div []
+                [ text "failure" ]
+
+
+
+-- Main
+
+
+main : Program () Model Msg
+main =
+    Browser.element
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
