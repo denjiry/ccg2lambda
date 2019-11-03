8af4db9d4c2988ecf55c3653249b2eae03f595ad
kento tajiri <k.denjiry@gmail.com>
2019-11-04 03:18:01 +0900

    Delete unnecessary code

--- server.py
+++ server.py
@@ -1,61 +1,10 @@
 import json as js
-from flask import Flask, send_from_directory, request, jsonify
+from flask import Flask, send_from_directory, request
 
 import operate as op
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False  # JSONでの日本語文字化け対策
-tasks = {
-    '1': 'スーパーで買い物をする',
-    '2': 'Netflixを見る'
-}
-
-
-@app.route('/hello')
-def hello_world():
-    return jsonify({'message': 'Hello, world'})
-
-
-@app.route('/foo/bar')
-def get_foobar():
-    return 'you got\n'
-
-
-@app.route('/foo/bar/<int:baz>', methods=['POST'])
-def post_foobar(baz):
-    return 'you posted {}\n'.format(baz)
-
-
-@app.route('/', methods=['POST'])
-def post_json():
-    json = request.get_json()  # POSTされたJSONを取得
-    return jsonify(json)  # JSONをレスポンス
-
-
-@app.route('/hoge', methods=['GET'])
-def get_json_from_dictionary():
-    dic = {
-        'foo': 'bar',
-        'ほげ': 'ふが'
-    }
-    return jsonify(dic)  # JSONをレスポンス
-
-
-@app.route('/tasks', methods=['GET'])
-def list_all_tasks():
-    json = {
-        'message': tasks
-    }
-    return jsonify(json)
-
-
-@app.route('/tasks/<int:taskid>', methods=['GET'])
-def show_task(taskid):
-    taskid = str(taskid)
-    json = {
-        'message': tasks[taskid]
-    }
-    return jsonify(json)
 
 
 @app.route('/api/delete', methods=['POST'])
